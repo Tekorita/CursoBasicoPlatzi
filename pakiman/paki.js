fdfd5516def6ce7f4b888f9274710cbab47e8ea5
Tekorita <davidpalmalugo@gmail.com>
2019-07-18 23:42:30 -0400

    implementacion y modulacione de clase en pakimanes

--- pakiman/paki.js
+++ pakiman/paki.js
@@ -3,16 +3,20 @@ imagenes['Cauchin'] = "https://firebasestorage.googleapis.com/v0/b/cursoplatzi-8
 imagenes['Pokacho'] = "https://firebasestorage.googleapis.com/v0/b/cursoplatzi-839b5.appspot.com/o/cerdo.webp?alt=media&token=21bca080-7c4d-488b-9571-05344454bced";
 imagenes['Tocinauro'] = "https://firebasestorage.googleapis.com/v0/b/cursoplatzi-839b5.appspot.com/o/cerdo.webp?alt=media&token=21bca080-7c4d-488b-9571-05344454bced";
 
-var cauchin = new Pakiman("Cauchin", 100, 30);
-var pokacho = new Pakiman("Pokacho", 80, 50);
-var tocinauro = new Pakiman("Tocinauro", 120, 40);
-
-pokacho.mostrar();
-cauchin.mostrar();
-tocinauro.mostrar();
-
 var coleccion = [];
 
-coleccion.push(cauchin);
-coleccion.push(pocacho);
-coleccion.push(tocinauro);
+coleccion.push(new Pakiman("Cauchin", 100, 30));
+coleccion.push(new Pakiman("Pokacho", 80, 50));
+coleccion.push(new Pakiman("Tocinauro", 120, 40));
+
+for(var freddito of coleccion)
+{
+    freddito.mostrar();
+}
+
+/*
+for(var freddito in coleccion)
+{
+    console.log(coleccion[freddito]);
+}
+*/
