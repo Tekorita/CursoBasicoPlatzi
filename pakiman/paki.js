7527e5319338017f46c64afa02b71c58818b1ca1
Tekorita <davidpalmalugo@gmail.com>
2019-07-18 23:43:33 -0400

    modificacion de pakimanes

--- pakiman/paki.js
+++ pakiman/paki.js
@@ -9,9 +9,9 @@ coleccion.push(new Pakiman("Cauchin", 100, 30));
 coleccion.push(new Pakiman("Pokacho", 80, 50));
 coleccion.push(new Pakiman("Tocinauro", 120, 40));
 
-for(var freddito of coleccion)
+for(var pakim of coleccion)
 {
-    freddito.mostrar();
+    pakim.mostrar();
 }
 
 /*
